525b6a6ca69094f6fe0138e0cc4a9e18ecba1372
caua-monteiro <c.monteiro1810@gmail.com>
2025-03-19 19:43:27 -0300

    Delete EX 2 directory

--- EX 2/src/ObjPessoa.java
+++
@@ -1,7 +0,0 @@
-public class ObjPessoa {
-    String Nome, Telfone;
-    public void ExibirDados(){
-        System.out.println("Nome: " + Nome);
-        System.out.println("Telefone: " + Telfone);
-    }
-}
--- EX 2/src/PessoaMain.java
+++
@@ -1,9 +0,0 @@
-public class PessoaMain {
-    public static void main(String[] args){
-        ObjPessoa Pessoa01 = new ObjPessoa();
-        Pessoa01.Nome = "Jacob Filho Jr";
-        Pessoa01.Telfone = "42 984043902";
-
-        Pessoa01.ExibirDados();
-    }
-}
