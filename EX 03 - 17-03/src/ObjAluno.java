18e689315b5238bf15c937eec83ee5cd2141f857
caua-monteiro <c.monteiro1810@gmail.com>
2025-03-19 19:43:16 -0300

    Delete EX 03 - 17-03 directory

--- EX 03 - 17-03/src/ObjAluno.java
+++
@@ -1,8 +0,0 @@
-public class ObjAluno {
-    String Nome, Curso;
-
-    void ExibeAluno(){
-        System.out.println("Aluno: " + Nome);
-        System.out.println("Curso: " + Curso);
-    }
-}
--- EX 03 - 17-03/src/ObjUniversidade.java
+++
@@ -1,8 +0,0 @@
-public class ObjUniversidade {
-    String Nome, Cidade, Estado;
-
-    void ExibeUniversidade(){
-        System.out.println("Universisdade: " + Nome);
-        System.out.println("Localizacao: " + Cidade + " - " + Estado);
-    }
-}
